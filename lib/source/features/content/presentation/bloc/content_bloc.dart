be4419f2d2519ce987fc6271c14a93c7cf88ef77
Mouhamad Douba <96375778+mouhamaddouba@users.noreply.github.com>
2025-12-10 02:56:06 +0300

    22. Make injection for content feature.

--- lib/source/features/content/presentation/bloc/content_bloc.dart
+++ lib/source/features/content/presentation/bloc/content_bloc.dart
@@ -1,11 +1,16 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:tecnical_task/source/features/content/data/repositories/content_repository.dart';
 
 part 'content_event.dart';
 part 'content_state.dart';
 
 class ContentBloc extends Bloc<ContentEvent, ContentState> {
-  ContentBloc() : super(ContentInitial()) {
+  final ContentRepository _contentRepository;
+
+  ContentBloc({required ContentRepository contentRepository})
+    : _contentRepository = contentRepository,
+      super(ContentInitial()) {
     on<ContentEvent>((event, emit) {
       // TODO: implement event handler
     });
--- lib/source/features/content/presentation/di/content_di.dart
+++ lib/source/features/content/presentation/di/content_di.dart
@@ -1,12 +1,22 @@
 import 'package:tecnical_task/source/core/dependencies/app_binding.dart';
 import 'package:tecnical_task/source/core/dependencies/app_dependencies.dart';
+import 'package:tecnical_task/source/core/services/api_service.dart';
+import 'package:tecnical_task/source/features/content/data/repositories/content_repository.dart';
 import 'package:tecnical_task/source/features/content/presentation/bloc/content_bloc.dart';
 
 class ContentDi extends BaseBinding {
   @override
   void dependencies() {
+    if (!instance.isRegistered<ContentRepository>()) {
+      instance.registerLazySingleton<ContentRepository>(
+        () => ContentRepositoryImpl(apiService: instance<ApiService>()),
+      );
+    }
+
     if (!instance.isRegistered<ContentBloc>()) {
-      instance.registerFactory<ContentBloc>(() => ContentBloc());
+      instance.registerFactory<ContentBloc>(
+        () => ContentBloc(contentRepository: instance<ContentRepository>()),
+      );
     }
   }
 
